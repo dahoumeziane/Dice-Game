38670e46c26294447649598b55c9cdd029169611
Meziane Dahou <hd_meziane@esi.dz>
2020-12-10 19:47:26 +0100

    Initial Commit

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by Dahou Meziane on 12/10/20.
+//  Copyright © 2020 Meziane Dahou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
